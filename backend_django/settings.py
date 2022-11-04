a162f643b37241566597680d56a3d99c56078c9e
roy752 <chun_sungwon@naver.com>
2022-11-04 13:35:32 +0900

    세팅의 DB 관련 코드 변경. 배포 에러 유발 방지.

--- backend_django/settings.py
+++ backend_django/settings.py
@@ -87,8 +87,8 @@ try:
 except:
     DATABASES = os.environ.get('DATABASE',None)
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+#db_from_env = dj_database_url.config(conn_max_age=500)
+#DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
